676f2c5b976e6b019e8470975967c2ab1cf92c2b
tangping <tangping@dustess.com>
2026-03-02 17:15:41 +0800

    middle

--- internal/interface/http/router/router.go
+++ internal/interface/http/router/router.go
@@ -1,6 +1,9 @@
 package router
 
 import (
+	"goKit/internal/interface/http/middleware"
+	"log/slog"
+
 	"github.com/gofiber/fiber/v2"
 	"go.uber.org/fx"
 )
@@ -12,6 +15,7 @@ type Router struct {
 
 type RouterIn struct {
 	fx.In
+	Logger *slog.Logger
 }
 
 // NewRouter 通过 Fx 依赖注入所有的 Handler
@@ -23,5 +27,8 @@ func NewRouter(par RouterIn) *Router {
 
 // Register 统一注册路由树
 func (r *Router) Register(app *fiber.App) {
+	// 全局 API 分组
+	v1 := app.Group("/api/v1")
+	v1.Use(middleware.ErrorHandler(r.params.Logger))
 
 }
